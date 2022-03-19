bfbd838dad3c72af6185684d04057f6633ea6e9d
Jennifer Varela <50208658+JenniferVarela@users.noreply.github.com>
2022-03-18 20:05:34 -0600

    ....

--- app/src/main/java/com/example/pm1examengrupo2/ActivityUsuario.java
+++ app/src/main/java/com/example/pm1examengrupo2/ActivityUsuario.java
@@ -1,28 +1,54 @@
 package com.example.pm1examengrupo2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.provider.Settings;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActivityUsuario extends AppCompatActivity {
 
-    Button btnSave,btnListarContactos;
+    Button btnSave,btnListarContactos,btnGaleria;
     FloatingActionButton btnTomarfoto;
     ImageView Foto;
     EditText txtNombre,txtTelefono,txtLat,txtLon;
+
+    Bitmap imagen;
+
+    static final int PETICION_ACCESO_CAM = 100;
+    static final int TAKE_PIC_REQUEST = 101;
+    static final int RESUL_GALLERY_IMG =200;
 
 
     @Override
@@ -39,14 +65,14 @@ public class ActivityUsuario extends AppCompatActivity {
         btnSave = (Button) findViewById(R.id.btnGuardar);
         btnListarContactos = (Button) findViewById(R.id.btnContactos);
         btnTomarfoto = (FloatingActionButton) findViewById(R.id.fbtnTomarFoto);
+        btnGaleria = (Button) findViewById(R.id.btnGaleria);
         txtNombre = (EditText) findViewById(R.id.txtNombre);
         txtTelefono = (EditText) findViewById(R.id.txtTelefono);
         txtLat = (EditText) findViewById(R.id.txtLat);
         txtLon = (EditText) findViewById(R.id.txtLon);
+        Foto = (ImageView) findViewById(R.id.imageView);
 
 
-
-        
 
         btnListarContactos.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,17 +85,139 @@ public class ActivityUsuario extends AppCompatActivity {
         btnTomarfoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                permisos();
+            }
+        });
 
+        btnGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GaleriaImagen();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendImage();
             }
         });
     }
 
+    private void permisos() {
 
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},PETICION_ACCESO_CAM);
+        }else{
+            tomarFoto();
+        }
+    }
 
+    private void tomarFoto() {
+        Intent takepic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        if(takepic.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(takepic,TAKE_PIC_REQUEST);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PETICION_ACCESO_CAM)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                tomarFoto();
+            }
+        }else{
+            Toast.makeText(getApplicationContext(),"Se necesitan permisos de acceso a la camara",Toast.LENGTH_LONG).show();
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        Byte [] arreglo;
+
+        if(requestCode == TAKE_PIC_REQUEST && resultCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            imagen = (Bitmap) extras.get("data");
+            Foto.setImageBitmap(imagen);
+        }
+
+        Uri imageUri;
+        if(resultCode==RESULT_OK && requestCode==RESUL_GALLERY_IMG)
+        {
+
+            imageUri = data.getData();
+            Foto.setImageURI(imageUri);
+            try {
+                imagen=MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
+
+            }catch (Exception e)
+            {
+
+            }
+        }
+
+    }
+
+    private String GetStringImage(Bitmap imagen) {
+
+        try {
+            ByteArrayOutputStream ba = new ByteArrayOutputStream();
+            imagen.compress(Bitmap.CompressFormat.JPEG, 70, ba);
+            byte[] imagebyte = ba.toByteArray();
+            String encode = Base64.encodeToString(imagebyte, Base64.DEFAULT);
+
+            return encode;
+        }catch (Exception ex)
+        {
+            ex.toString();
+        }
+        return "";
+    }
+
+    private void GaleriaImagen()
+    {
+        Intent intentGaleria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intentGaleria,RESUL_GALLERY_IMG);
+    }
+
+    private void SendImage() {
+
+/*        StringRequest stringRequest = new StringRequest(Request.Method.POST, RestApiMethods.EndPointImage, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Respuesta",response.toString());
+                Toast.makeText(getApplicationContext(),"imagen SUbida" + response.toString(),Toast.LENGTH_LONG).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Respuesta",error.toString());
+                Toast.makeText(getApplicationContext(),"Error " + error.toString(),Toast.LENGTH_LONG).show();
+            }
+        }) {
+            //data que se envia mediante request
+            protected Map<String, String> getParams() {
+
+                String image = GetStringImage(imagen);
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("imagen", image);
+
+                return parametros;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);*/
+    }
 
 
 }
